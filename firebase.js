c3806b4c421576837bdb1bbbfd75dee690d3ae53
ropt8878 <140503149+ropt8878@users.noreply.github.com>
2023-07-27 10:40:39 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD9lauOnkqYQ_iC7rTBLtIrXgj0-w_vVGo",
+    authDomain: "sdg5-hhv.firebaseapp.com",
+    databaseURL: "https://sdg5-hhv-default-rtdb.firebaseio.com",
+    projectId: "sdg5-hhv",
+    storageBucket: "sdg5-hhv.appspot.com",
+    messagingSenderId: "1006454697293",
+    appId: "1:1006454697293:web:8c39779912b62a213a9b04"
+};
+
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
